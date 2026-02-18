39bd6a75015ed7416cc7335d6a97ee4376c0dad7
faith <faith.henu@student.moringaschool.com>
2026-02-18 21:18:32 +0300

    Path and query parameters

--- routers/blog_post.py
+++ routers/blog_post.py
@@ -1,10 +1,22 @@
 from fastapi import APIRouter
+from typing import Optional
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix='/blog',
     tags=['blog']
 )
 
-@router.post('/new')
-def create_blog():
-    pass
+class BlogModel(BaseModel):
+    title: str
+    content: str
+    no_of_comments: int
+    published: Optional[bool]
+
+@router.post('/new/{id}')
+def create_blog(blog: BlogModel, id = int, version: int= 1):
+    return {
+        'id': id,
+        'version': version,
+        'data': blog
+        }
