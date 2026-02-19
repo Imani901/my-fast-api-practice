5e354c522fc137723a14c43dfc657c527016a659
faith <faith.henu@student.moringaschool.com>
2026-02-19 09:30:30 +0300

    Parameter metadata

--- routers/blog_post.py
+++ routers/blog_post.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query, Body
 from typing import Optional
 from pydantic import BaseModel
 
@@ -20,3 +20,28 @@ def create_blog(blog: BlogModel, id = int, version: int= 1):
         'version': version,
         'data': blog
         }
+
+
+@router.post('/new/{id}/comment')      
+def create_comment(blog: BlogModel,
+                    id:int, 
+                    comment_id:int= Query(None,
+                    title= 'Id of the comment',
+                    description='Some decription for comment_id',
+                    alias='commentId',
+                    deprecated= True
+                    ),
+                    content: str= Body(...,
+                                       min_length=10,
+                                       max_length=20
+                                       )
+  
+                    ):
+    return{
+        'blog': blog,
+        'id': id,
+        'comment_id': comment_id,
+        'content': content
+    }
+    
+    
